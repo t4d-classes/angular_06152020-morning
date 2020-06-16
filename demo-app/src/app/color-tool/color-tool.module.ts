d31461ecb60fc26e4530f8fee3064e0b08cd84f6
Eric Greene <eric@t4d.io>
2020-06-16 09:50:59 -0400

    add exercise 4

--- demo-app/src/app/color-tool/color-tool.module.ts
+++ demo-app/src/app/color-tool/color-tool.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from '../shared/shared.module';
 
 import { ColorHomeComponent } from './components/color-home/color-home.component';
 
@@ -12,7 +13,7 @@ import { ColorHomeComponent } from './components/color-home/color-home.component
   // template within this module
   declarations: [ColorHomeComponent],
   imports: [
-    CommonModule, ReactiveFormsModule,
+    CommonModule, ReactiveFormsModule, SharedModule,
   ],
   // allow other modules to use this component in their
   // templates
