5059a184e1207b7545ae806ac5c634ab54af1701
Eric Greene <eric@t4d.io>
2020-06-17 12:01:52 -0400

    add exercises

--- demo-app/exercises/exercise-7.md
+++ demo-app/exercises/exercise-7.md
@@ -5,3 +5,9 @@
 2. Utilize the Cars service on Car Home only. Remove all of the code which is replaced by the Cars service functionality.
 
 3. Ensure it works!
+
+Bonus
+
+1. Move the color list to its own component, then use the color list component in Car Home.
+
+2. Update the colors service to sort the list of colors by name.
--- demo-app/src/app/color-tool/color-tool.module.ts
+++ demo-app/src/app/color-tool/color-tool.module.ts
@@ -6,13 +6,15 @@ import { SharedModule } from '../shared/shared.module';
 
 import { ColorHomeComponent } from './components/color-home/color-home.component';
 import { ColorFormComponent } from './components/color-form/color-form.component';
+import { MyuppercasePipe } from './pipes/myuppercase.pipe';
+import { MyappendPipe } from './pipes/myappend.pipe';
 
 
 
 @NgModule({
   // anything registered here can be access within a 
   // template within this module
-  declarations: [ColorHomeComponent, ColorFormComponent],
+  declarations: [ColorHomeComponent, ColorFormComponent, MyuppercasePipe, MyappendPipe],
   imports: [
     CommonModule, ReactiveFormsModule, SharedModule,
   ],
